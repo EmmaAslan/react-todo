3e60e853921bc9d43af8598f8980b47eabaabd1d
Emma Kanewoff <emmakanewoff@gmail.com>
2023-01-24 16:03:35 +0100

    Commit 1

---
+++ src/components/Todos.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import { FaCheck, FaTrash } from "react-icons/fa";
+
+const Todos = () => {
+  const [todoList, setTodoList] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const addTodo = () => {
+    let newTodoItems = [...todoList];
+
+    let newTodoItem = {
+      id: Date.now(),
+      label: newTodo,
+      complete: false,
+    };
+
+    newTodoItems.push(newTodoItem);
+    setTodoList(newTodoItems);
+  };
+
+  console.log("todoList", todoList);
+
+  let todoValue = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodoList2 = [...todoList];
+    updatedTodoList2.forEach((todo) => {
+      if (todo.id === id) {
+        todo.complete = true;
+      }
+    });
+    setTodoList(updatedTodoList2);
+  };
+
+  const deleteTodo = (id) => {
+    let updatedTodoList = todoList.filter((todo) => todo.id !== id);
+    setTodoList(updatedTodoList);
+  };
+
+  return (
+    <div className="container">
+      <h1>Todo List</h1>
+
+      {todoList.map((todo) => (
+        <div className="todoList">
+          <ul>
+            <li key={todo.id} className="list">
+              <span className={todo.complete === true && "true"}>
+                {todo.complete === false ? "false" : "true"} - {todo.label}
+              </span>
+              <div className="todoButtons">
+                {todo.complete === true ? (
+                  <button disabled className="disabled">
+                    <FaCheck />
+                  </button>
+                ) : (
+                  <button
+                    className="activeButton"
+                    onClick={() => {
+                      completeTodo(todo.id);
+                    }}
+                  >
+                    <FaCheck />
+                  </button>
+                )}
+
+                <button
+                  className="activeButton"
+                  onClick={() => {
+                    deleteTodo(todo.id);
+                  }}
+                >
+                  <FaTrash />
+                </button>
+              </div>
+            </li>
+          </ul>
+        </div>
+      ))}
+
+      <div className="newTodo">
+        <h2>New todo</h2>
+        <div className="inputButton">
+          <input
+            type="text"
+            id="todo"
+            placeholder="New todo"
+            onChange={todoValue}
+          />
+          {newTodo.length ? (
+            <button className="activeAdd" onClick={addTodo}>
+              Add
+            </button>
+          ) : (
+            <button disabled>Add</button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Todos;
